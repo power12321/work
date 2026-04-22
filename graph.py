d5a2689db6f8bc82075ea53d2806e161e36fa38b
tpw <3368199292@qq.com>
2026-04-23 00:40:24 +0800

    add

--- graph.py
+++ graph.py
@@ -33,11 +33,11 @@ def plot_all(title, pred_data, orig_data, dates):
 
 print(f"numpy: {np.__version__}")
 print(f"pandas: {pd.__version__}")
-file_path = 'output145CEA.csv'
+file_path = 'output144hubei.csv'
 df_pred = pd.read_csv(file_path)
 pred_data = df_pred.iloc[:, 1].values
 
-demo_path = 'CEAdemo_data.csv'
+demo_path = 'hubeidemo_data.csv'
 if os.path.exists(demo_path):
     df_orig = pd.read_csv(demo_path, header=None)
     
